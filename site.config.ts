3248f83a799d845e48d78eb9590ae177833fc6b2
Geetansh Agrawal <agrawalgeetansh810@gmail.com>
2025-05-21 00:31:17 +0530

    Github Deployment changes

--- site.config.ts
+++ site.config.ts
@@ -1,5 +1,5 @@
 export const siteUrl = "https://geetansh810.github.io";
-export const siteTitle = "Sqratch";
+export const siteTitle = "Sqratch.in";
 export const defaultLocale = "en";
 export const locales = ["en", "it"];
 export type Locale = "en" | "it";
